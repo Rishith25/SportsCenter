81ff636901318c3900f5e2939e6a5ca6d1eaca26
rishith <kamishetty.rishith@gmail.com>
2023-10-25 08:49:10 +0530

    Sports Fetched

---
+++ src/context/sports/context.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { createContext, useContext, useReducer } from "react";
+import { reducer, initialState, SportsState, SportsActions } from "./reducer";
+
+const SportsStateContext = createContext<SportsState | undefined>(undefined);
+
+type SportsDispatch = React.Dispatch<SportsActions>;
+const SportsDispatchContext = createContext<SportsDispatch | undefined>(
+  undefined
+);
+
+export const SportsProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <SportsStateContext.Provider value={state}>
+      <SportsDispatchContext.Provider value={dispatch}>
+        {children}
+      </SportsDispatchContext.Provider>
+    </SportsStateContext.Provider>
+  );
+};
+export const useSportsState = () => useContext(SportsStateContext);
+export const useSportsDispatch = () => useContext(SportsDispatchContext);
---
+++ src/context/sports/reducer.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+interface Sport {
+  id: number;
+  name: string
+}
+
+export const initialState: SportsState = {
+  sports: [],
+  isLoading: false,
+  isError: false,
+  errorMessage: "",
+};
+
+export interface SportsState {
+  sports: Sport[];
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string;
+}
+
+export type SportsActions =
+  | { type: "FETCH_SPORTS_REQUEST" }
+  | { type: "FETCH_SPORTS_SUCCESS"; payload: Sport[] }
+  | { type: "FETCH_SPORTS_FAILURE"; payload: string }
+
+export const reducer = (
+  state: SportsState = initialState,
+  action: SportsActions
+): SportsState => {
+  switch (action.type) {
+    case "FETCH_SPORTS_REQUEST":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "FETCH_SPORTS_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        sports: action.payload,
+      };
+    case "FETCH_SPORTS_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        errorMessage: action.payload,
+      };
+    default:
+      return state;
+  }
+};
