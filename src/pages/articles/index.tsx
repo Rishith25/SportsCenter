f72f579cc5450fb718c8d427ba42c8ef6855165e
rishith <kamishetty.rishith@gmail.com>
2023-10-25 08:53:30 +0530

    Artilces Index Page

---
+++ src/pages/articles/index.tsx
@@ -0,0 +1,13 @@
+import ArticlesList from "./ArticlesList";
+import FilterArticles from "./FilterArticles";
+const Articles = () => {
+  return (
+    <>
+    <div className="">
+        {/* <FilterArticles /> */}
+      </div>
+      <ArticlesList />
+    </>
+  )
+}
+export default Articles;
