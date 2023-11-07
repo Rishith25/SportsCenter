1de62f2cb1b95f0f87d1f664c4a874e94674b66d
rishith <kamishetty.rishith@gmail.com>
2023-11-07 19:41:39 +0530

    Articles Favorites added and error resolved

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a77g5kaoo7o"
+    "revision": "0.3ai8ladqttg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/articles/ArticlesListItems.tsx
+++ src/pages/articles/ArticlesListItems.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -37,11 +38,6 @@ export default function ArticlesListItems({ selectedSport }: any) {
   const { sports, isLoading1, isError1, errorMessage1 } = Sportsstate;
 
   const { preferences, isLoading2, isError2, errorMessage2 } = Preferencesstate;
-
-  const initialSelectedArticles = preferences.articles || [];
-  const [favouriteArticles, setSelectedArticles] = useState(
-    initialSelectedArticles
-  );
 
   let filterArticles = null;
   if (isAuthenticated) {
@@ -92,6 +88,15 @@ export default function ArticlesListItems({ selectedSport }: any) {
     setIsOpen(true);
   };
 
+  
+  let initialSelectedArticles = [];
+  if (preferences && preferences.articles) {
+    initialSelectedArticles = preferences.articles || []
+  }
+  let [favouriteArticles, setSelectedArticles] = useState(
+    initialSelectedArticles
+  );
+
   // eslint-disable-next-line react-hooks/rules-of-hooks
   // useEffect(() => {
   //   if (preferences && preferences.sports && preferences.teams) {
@@ -99,9 +104,12 @@ export default function ArticlesListItems({ selectedSport }: any) {
   //   }
   // }, [preferences]);
 
+  
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const token = localStorage.getItem("authToken") ?? "";
+
     const existingPreferences = {
       sports: preferences.sports,
       teams: preferences.teams,
@@ -146,18 +154,20 @@ export default function ArticlesListItems({ selectedSport }: any) {
   };
 
   const handleToggleFavorite = (id: number) => {
-    const isSaved = favouriteArticles.includes(id);
-    if (isSaved) {
-      // The article is already in favorites, so remove it by its ID
-      const updatedArticleIds = favouriteArticles.filter(
-        (articleId) => articleId !== id
-      );
-      setSelectedArticles(updatedArticleIds);
-      console.log("Removed from favorites", updatedArticleIds);
-    } else {
-      // The article is not in favorites, so add it by its ID
-      setSelectedArticles([...favouriteArticles, id]);
-      console.log("Added to favorites", [...favouriteArticles, id]);
+    if (isAuthenticated) {
+      const isSaved = favouriteArticles.includes(id);
+      if (isSaved) {
+        // The article is already in favorites, so remove it by its ID
+        const updatedArticleIds = favouriteArticles.filter(
+          (articleId: number) => articleId !== id
+        );
+        setSelectedArticles(updatedArticleIds);
+        console.log("Removed from favorites", updatedArticleIds);
+      } else {
+        // The article is not in favorites, so add it by its ID
+        setSelectedArticles([...favouriteArticles, id]);
+        console.log("Added to favorites", [...favouriteArticles, id]);
+      }
     }
   };
 
@@ -186,44 +196,46 @@ export default function ArticlesListItems({ selectedSport }: any) {
                       </p>
                       {/* Add date and time here */}
                     </div>
-                    <div className="flex">
-                      <form onSubmit={handleSubmit}>
-                        {isAuthenticated && (
-                          <button
-                            value={article.id}
-                            data-article-id={article.id}
-                            type="submit"
-                            onClick={() => handleToggleFavorite(article.id)}
-                          >
-                            {favouriteArticles.includes(article.id) ? (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="red"
-                                className="w-6 h-6"
-                              >
-                                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-                              </svg>
-                            ) : (
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-6 h-6"
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                                />
-                              </svg>
-                            )}
-                          </button>
-                        )}
-                      </form>
-                    </div>
+                    {isAuthenticated ? (
+                      <div className="flex">
+                        <form onSubmit={handleSubmit}>
+                          {isAuthenticated ? (
+                            <button
+                              value={article.id}
+                              data-article-id={article.id}
+                              type="submit"
+                              onClick={() => handleToggleFavorite(article.id)}
+                            >
+                              {favouriteArticles.includes(article.id) ? (
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 24 24"
+                                  fill="red"
+                                  className="w-6 h-6"
+                                >
+                                  <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                                </svg>
+                              ) : (
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  className="w-6 h-6"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                                  />
+                                </svg>
+                              )}
+                            </button>
+                          ) : null}
+                        </form>
+                      </div>
+                    ) : null}
                   </div>
                 </div>
 
